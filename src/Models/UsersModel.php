d5b8824f4aaa095688fa884a599f742610f8c1fa
Dedenis <gaeldedenis01@laposte.net>
2021-03-30 08:37:59 +0200

    Création UsersModel.php

---
+++ src/Models/UsersModel.php
@@ -0,0 +1,13 @@
+<?php
+
+    namespace App\Models;
+
+    use App\Models\MainModel;
+
+    /**
+    * Class UsersModel
+    * @package App\Models
+    */
+    class UsersModel extends MainModel {
+
+    }
