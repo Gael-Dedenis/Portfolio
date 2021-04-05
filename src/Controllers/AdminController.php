2fc1fad1d6122314177cfbb61b318c2d67485ea5
Dedenis <gaeldedenis01@laposte.net>
2021-04-05 15:51:28 +0200

    Correction sur l'AdminController

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -23,9 +23,10 @@
             if ($this->getUser() === false) {
                 $this->redirect("auth");
             }
-
             $allProjects = ModelsFactory::getModel("Projects")->listData();
+            $mailUser    = (string) $this->session["user"]["mail"];
+            $user        = ModelsFactory::getModel("Users")->readData($mailUser, "mail");
 
-            return $this->render("admin.twig", ["projects" => $allProjects]);
+            return $this->render("admin.twig", ["projects" => $allProjects, "user" => $user]);
         }
     }
