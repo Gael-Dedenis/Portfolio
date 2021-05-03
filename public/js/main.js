e7cecee5f4317b694d74a5fce37d229b2044cac5
Dedenis <gaeldedenis01@laposte.net>
2021-05-03 23:54:15 +0200

    Edition du main.js pour appeler la création d'un objet form_contact

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// attente de la fin de chargement de la fenêtre.
+window.onload = () => {
+
+    // +++++ +++++ Formulaire de contact +++++ +++++
+    let form_Contact = new FormContact(document.getElementById("contact"));
+};
