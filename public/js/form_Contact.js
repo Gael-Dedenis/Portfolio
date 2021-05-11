6920be7654c8a742f86f9d4c3832b1a7dfd2a484
Dedenis <gaeldedenis01@laposte.net>
2021-05-11 08:45:35 +0200

    Corrections et ajouts de fonction dans form_Contact.js

--- public/js/form_Contact.js
+++ public/js/form_Contact.js
@@ -1,7 +1,6 @@
 "use strict";
 
 class FormContact {
-
     /**
      * @param {HTMLElement} container Id du formulaire de contacts
      */
@@ -15,47 +14,54 @@ class FormContact {
     setContactEvents() {
         this.container.addEventListener("submit", (e) => {
             e.preventDefault();
-            this.setPromiseAjax()
+
+            this.setPreventEmptyInputForm("form_contact", "submit", [
+                "nom",
+                "mail",
+                "message",
+            ]);
+
+            this.setAjax();
         });
+    }
+
+    // +++++ +++++ Récupération des données +++++ +++++
+    async getData() {
+        let data         = {};
+        let inputName    = document.getElementById("nom");
+        let inputMail    = document.getElementById("mail");
+        let inputMessage = document.getElementById("message");
+
+        data = {
+            nom: inputName.value,
+            mail: inputMail.value,
+            message: inputMessage.value
+        };
+
+        return data;
     }
 
     // +++++ +++++ Requête Ajax +++++ +++++
-    setPromiseAjax() {
-        this.requestAjax = new Ajax("https://localhost/portfolio/public/index.php?access=contact");
-        this.requestAjax.then(function (xhr) {
-            if (xhr.status !== 200) {
-                console.log("Erreur lors de la requête Ajax.");
-            } else {
-                console.log("Envoie réussi !");
-            }
-        }).catch(function (error) {
-            console.error(error);
-        })
+    async setAjax() {
+        console.log(this.getData());
+        let requestAjax = new Ajax("https://localhost/portfolio/public/index.php?access=contact", "POST", this.getData());
     }
-}
 
-
-/* window.onload = () => {
-    preventEmptyInputForm("form_contact", "submit", [
-        "nom",
-        "mail",
-        "message",
-    ]);
-};
-
-function preventEmptyInputForm(formId, eventType, inputsId) {
+    // +++++ +++++ Vérification que les inputs ne sont pas vide.
+    async setPreventEmptyInputForm(formId, eventType, inputsId) {
     const form = document.getElementById(formId);
 
-    if (form) {
-        form.addEventListener(eventType, (e) => {
-            inputsId.forEach((inputId) => {
-                const input =document.getElementById(inputId);
-                if(input.nodeValue.trim().length === 0) {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    return alert("Le champ " + inputId + " n\'est pas rempli !");
-                };
+        if (form) {
+            form.addEventListener(eventType, (e) => {
+                inputsId.forEach((inputId) => {
+                    const input = document.getElementById(inputId);
+                    if(input.nodeValue.trim().length === 0) {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        return alert("Le champ " + inputId + " n\'est pas rempli !");
+                    };
+                });
             });
-        });
+        }
     }
-} */
+}
