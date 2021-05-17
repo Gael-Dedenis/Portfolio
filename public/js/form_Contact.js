16f7c2bdedba524b1e885f54b5883c80b8a355f6
Dedenis <gaeldedenis01@laposte.net>
2021-05-17 22:51:27 +0200

    Suppression de l'apel Ajax sur le submit du formulaire de contact

--- public/js/form_Contact.js
+++ public/js/form_Contact.js
@@ -13,42 +13,17 @@ class FormContact {
     // +++++ Ajouts des évènements sur le formulaire de contact
     setContactEvents() {
         this.container.addEventListener("submit", (e) => {
-            e.preventDefault();
-
-            this.setPreventEmptyInputForm("form_contact", "submit", [
+            this.checkEmptyInput("form_contact", "submit", [
                 "nom",
                 "mail",
                 "message",
             ]);
 
-            this.setAjax();
         });
     }
 
-    // +++++ +++++ Récupération des données +++++ +++++
-    async getData() {
-        let data         = {};
-        let inputName    = document.getElementById("nom");
-        let inputMail    = document.getElementById("mail");
-        let inputMessage = document.getElementById("message");
-
-        data = {
-            nom: inputName.value,
-            mail: inputMail.value,
-            message: inputMessage.value
-        };
-
-        return data;
-    }
-
-    // +++++ +++++ Requête Ajax +++++ +++++
-    async setAjax() {
-        console.log(this.getData());
-        let requestAjax = new Ajax("https://localhost/portfolio/public/index.php?access=contact", "POST", this.getData());
-    }
-
     // +++++ +++++ Vérification que les inputs ne sont pas vide.
-    async setPreventEmptyInputForm(formId, eventType, inputsId) {
+    async checkEmptyInput(formId, eventType, inputsId) {
     const form = document.getElementById(formId);
 
         if (form) {
