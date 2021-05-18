da037bbeb0b8b52ccad184b069f8650ec0bb3bfa
Dedenis <gaeldedenis01@laposte.net>
2021-05-18 08:59:33 +0200

    Ajout de la création d'objet du bouton Loadmore

--- public/js/main.js
+++ public/js/main.js
@@ -5,4 +5,7 @@ window.onload = () => {
 
     // +++++ +++++ Formulaire de contact +++++ +++++
     let formContact = new FormContact(document.getElementById("form_contact"));
+
+    // +++++ +++++ Bouton Loadmore +++++ +++++
+    let buttonLoadmore = new Loadmore(document.getElementById("loadmore"), "https://localhost/portfolio/public/index.php?access=portfolio", "POST");
 };
