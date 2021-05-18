7c96c81a0139fd8f238abdb34d53c932fd33d793
Dedenis <gaeldedenis01@laposte.net>
2021-05-18 08:25:48 +0200

    Modification du contact controller

--- src/Controllers/ContactController.php
+++ src/Controllers/ContactController.php
@@ -16,6 +16,11 @@
     class ContactController extends MainController
     {
         /**
+         * @var
+         */
+        protected $eventMessage = [];
+
+        /**
          * @return string
          * @throws LoaderError
          * @throws RuntimeError
@@ -26,9 +31,11 @@
             {
                 $this->redirect("home");
             }
-            $this->sendMethod();
-            $this->redirect("home");
 
+            $this->sendMethod();
+            $this->eventMessage["success"] = "true";
+            $this->eventMessage["message"] = "Message envoyé !";
+            $this->redirect("home", ["eventContact" => $this->eventMessage]);
         }
 
         /**
@@ -37,26 +44,33 @@
          */
         private function sendMethod()
         {
-            // Création du transport
-            $transport = (new Swift_SmtpTransport())
-                ->setHost(MAIL_HOST)
-                ->setPort(MAIL_PORT)
-                ->setUsername(MAIL_USERNAME)
-                ->setPassword(MAIL_PASS)
-                ;
+            try {
+                // Création du transport
+                $transport = (new Swift_SmtpTransport())
+                    ->setHost(MAIL_HOST)
+                    ->setPort(MAIL_PORT)
+                    ->setUsername(MAIL_USERNAME)
+                    ->setPassword(MAIL_PASS)
+                    ;
 
-            // Création du mailer utilisant le transport
-            $mailer = new Swift_Mailer($transport);
+                // Création du mailer utilisant le transport
+                $mailer = new Swift_Mailer($transport);
 
-            // Création du message
-            $message = (new Swift_Message())
+                // Création du message
+                $message = (new Swift_Message())
 
-                ->setFrom([MAIL_FROM => "Gael Dedenis Portfolio"])
-                ->setTo([MAIL_TO => "J.forteroche", htmlspecialchars($this->post["mail"]) => htmlspecialchars($this->post["nom"])])
-                ->setBody(htmlspecialchars($this->post["message"]))
-                ;
+                    ->setFrom([MAIL_FROM => "Gael Dedenis Portfolio"])
+                    ->setTo([MAIL_TO => "Gael Dedenis - Portfolio", htmlspecialchars($this->post["mail"]) => htmlspecialchars($this->post["nom"])])
+                    ->setBody(htmlspecialchars($this->post["message"]))
+                    ;
 
-            // envoie du mail
-            return $mailer->send($message);
+                // envoie du mail
+                return $mailer->send($message);
+            } catch (Exception $e) {
+                $this->eventMessage["success"] = "false";
+                $this->eventMessage["message"] = "Erreur lors de l'envoi :" . $e;
+                $this->redirect("home", ["eventContact" => $this->eventMessage]);
+            }
+
         }
     }
