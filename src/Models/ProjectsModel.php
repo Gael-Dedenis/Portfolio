305fcdeb66c64ca578bbac1ed32d3e7bb19e07b8
Dedenis <gaeldedenis01@laposte.net>
2021-05-25 09:00:01 +0200

    Ajout d'une méthode dédier à récuperer seulement les 4 Derniers projets

--- src/Models/ProjectsModel.php
+++ src/Models/ProjectsModel.php
@@ -9,5 +9,12 @@
     * @package App\Models
     */
     class ProjectsModel extends MainModel {
+        /**
+         * requête spéciale pour récuperer uniquement les 4 derniers projets.
+         */
+        public function get4LastProjects() {
+            $query = "SELECT * FROM projects ORDER BY id DESC LIMIT 4";
 
+            return $this->database->getAllData($query);
+        }
     }
