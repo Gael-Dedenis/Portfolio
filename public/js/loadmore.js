1df0b69ac512f1f976ecf499042c75b337587381
Dedenis <gaeldedenis01@laposte.net>
2021-05-25 08:59:12 +0200

    Ajout de la création d'éléments + récup des données de Fetch

--- public/js/loadmore.js
+++ public/js/loadmore.js
@@ -4,24 +4,43 @@ class Loadmore {
 
     /**
      * @param {HTMLElement} button
+     *  @param {string} url
+     *  @param {string} method
      */
-    constructor(button, url, method) {
+    constructor(button, url) {
         this.button = button;
         this.url    = url;
-        this.method = method;
 
-        console.log(this.button, this.url, this.method);
         this.setEventUi();
     }
 
     setEventUi() {
-        this.button.addEventListener("submit", (e) => {
-            console.log("submit ok !");
-            this.setNewRequest();
+        this.getEventButton();
+    }
+
+    getEventButton() {
+        this.button.addEventListener("click", (e) => {
+            let newRequest = new Ajax(this.url);
+            console.log(newRequest);
+            this.addProjects(newRequest);
         });
     }
 
-    async setNewRequest() {
-        let newRequest = new Ajax(this.url, this.method);
+    addProjects(data) {
+        let container = document.getElementById("container-portfolio");
+/*         let card      = this.createElmtWithClass("div", ["my-3 px-5 text-center project"]);
+        let title     = this.createElmtWithClass("h3", ["text-decoration-underline fw-bold"]);
+        let link      = this.createElmtWithClass("a", [""]); */
     }
+
+    createElmtWithClass(type, className) {
+        let element = document.createElement(type);
+
+        if (className !== "" || className !== null || className !== "undefined" || className !== []) {
+            element.setAttribute("class", className);
+        }
+
+        return element;
+    }
+
 }
