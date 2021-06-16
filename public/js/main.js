8eef741a523438aaba098a3c05de8b5666150f99
Dedenis <gaeldedenis01@laposte.net>
2021-06-16 15:36:13 +0200

    Appel la création d'un objet checkForms

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,9 @@
 // attente de la fin de chargement de la fenêtre.
 window.onload = () => {
 
+    // +++++ +++++ Check Formulaire de contact +++++ +++++
+    let form = new checkForms(document.getElementById("form_contact"));
+
     // +++++ +++++ Bouton Loadmore +++++ +++++
     let button_Loadmore = new Loadmore(document.getElementById("loadmore"), "https://localhost/portfolio/public/index.php?access=portfolio", document.getElementById("container-portfolio"));
 
